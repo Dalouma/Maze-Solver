5edb96c1afa15edfdce28ff4010b061c964f2f26
Dalouma <david.amaya20@yahoo.com>
2025-05-31 15:26:03 -0400

    Defined Cell class in maze.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from graphics import Window, Line, Point
+from maze import Cell
 
 def main():
     win = Window(800, 800)
-    line = Line(Point(200,200), Point(600,600))
-    win.draw_line(line)
+    # line = Line(Point(200,200), Point(600,600))
+    # win.draw_line(line)
+    cell = Cell(win)
+    cell.draw(100, 100, 200, 200)
+    cell2 = Cell(win)
+    cell2.draw(150, 225, 525, 675)
+    cell3 = Cell(win)
+    cell3.draw(10, 10, 790, 790)
 
     # This runs the window after settings have been applied.
     # Window appearance must be determined before this is called.
