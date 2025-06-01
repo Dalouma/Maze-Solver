89d7f8d857bcbbae62d205d0778ba8c27aec38fb
Dalouma <david.amaya20@yahoo.com>
2025-05-31 20:28:15 -0400

    Maze can animate cells

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from graphics import Window, Line, Point
-from maze import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 800)
     # line = Line(Point(200,200), Point(600,600))
     # win.draw_line(line)
-    cell = Cell(win)
-    cell.draw(100, 100, 200, 200)
-    cell2 = Cell(win)
-    cell2.draw(150, 225, 525, 675)
-    cell3 = Cell(win)
-    cell3.draw(10, 10, 790, 790)
-    cell.draw_move(cell2)
-    cell.draw_move(cell3, True)
+    # cell = Cell(win)
+    # cell.draw(100, 100, 200, 200)
+    # cell2 = Cell(win)
+    # cell2.draw(150, 225, 525, 675)
+    # cell3 = Cell(win)
+    # cell3.draw(10, 10, 790, 790)
+    # cell.draw_move(cell2)
+    # cell.draw_move(cell3, True)
+    maze = Maze(100, 100, 20, 30, 20, 20, win)
+    maze._animate()
 
     # This runs the window after settings have been applied.
     # Window appearance must be determined before this is called.
--- maze.py
+++ maze.py
@@ -1,8 +1,9 @@
 from graphics import Window, Line, Point
+import time
 
 
 class Cell():
-    def __init__(self, window: Window):
+    def __init__(self, window=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -21,6 +22,9 @@ class Cell():
         self.__x2 = x2
         self.__y2 = y2
 
+        if self.__win is None:
+            return
+
         if self.has_left_wall:
             self.__win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
         if self.has_right_wall:
@@ -34,4 +38,52 @@ class Cell():
         color = "gray" if undo else "red"
         my_center = Point((self.__x1+self.__x2)/2, (self.__y1+self.__y2)/2)
         other_center = Point((to_cell.__x1+to_cell.__x2)/2, (to_cell.__y1+to_cell.__y2)/2)
+
+        if self.__win is None:
+            return
+
         self.__win.draw_line(Line(my_center, other_center), color)
+
+class Maze():
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        self.__cells = [ [Cell(self.__win) for _ in range(self.__num_rows)] for _ in range(self.__num_cols) ]
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
+    
+    def __draw_cell(self, i, j):
+        x1 = i * self.__cell_size_x + self.__x1
+        y1 = j * self.__cell_size_y + self.__y1
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+    
+    def _animate(self):
+        if self.__win is None:
+            return
+
+        self.__win.redraw()
+        time.sleep(0.01)
+        
